0b75458d0fa9784d23f839525f47a99d07906b2a
Meliodas26 <aldogtz26798@gmail.com>
2021-01-12 19:23:52 -0600

    headerANDfooter-responsive

--- MilesChristi/header.php
+++ MilesChristi/header.php
@@ -1,10 +1,13 @@
-<header>
-		<a class="component" href="./"><img src="./assets/img/logo.png" alt="logo"></a>
-		<div class="component">
+<header id="header">
+	<div class="top">
+		<div class="logo">
+			<a class="component" href="./"><img src="./assets/img/logo.png" alt="logo"></a>
+		</div>
+		<div class="component component-mobile">
 			<div class="social-networks">
-				<div><a href="#"><i class="fab fa-youtube"></i></a></div>
-				<div><a href="#"><i class="fab fa-instagram"></i></a></div>
-				<div><a href="#"><i class="fab fa-facebook-f"></i></a></div>
+				<a href="#"><div><i class="fab fa-youtube"></i></div></a>
+				<a href="#"><div><i class="fab fa-instagram"></i></div></a>
+				<a href="#"><div><i class="fab fa-facebook-f"></i></div></a>
 			</div>
 			<nav>
 				<ul>
@@ -16,5 +19,28 @@
 					<li><a href="#">Donaciones</a></li>
 				</ul>
 			</nav>
+			<!-- Enlace para ver menu en dispositivos mobiles -->
+			<div class="view-nav-mobile", style="display:none;">
+				<a onClick="view_navbar()"><i class="fas fa-bars"></i></a>
+			</div>
 		</div>
-	</header>
+	</div>
+	<div class="bottom">
+		<h1>Lorem Ipsum Dolor</h1>
+		<div><h3>Sit cupidatat nisi aliqua adipisicing culpa sunt id veniam. Consectetur amet ea dolor laborum sit aliqua dolor nulla anim.</h3></div>
+	</div>
+</header>
+
+<div id="nav-mobile" style="display:none;">
+	<div><a onClick="view_navbar()"><i class="fas fa-times"></i></a></div>	
+	<ul>
+		<a href="#"><li>Inicio</li></a>
+		<a href="#"><li>Blog</li></a>
+		<a href="#"><li>Ejercicios espirituales</li></a>
+		<a href="#"><li>Jóvenes</li></a>
+		<a href="#"><li>Contacto</li></a>
+		<a href="#"><li>Donaciones</li></a>
+	</ul>
+</div>
+
+<script src="assets/js/control-views.js"></script>
