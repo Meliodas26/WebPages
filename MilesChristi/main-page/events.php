53e12f3ca70c62fd999dc628a01596c146172845
Meliodas26 <aldogtz26798@gmail.com>
2021-01-18 19:49:25 -0600

    details

--- MilesChristi/main-page/events.php
+++ MilesChristi/main-page/events.php
@@ -1,11 +1,48 @@
+<section class="events generic-2">
+	<div class="title">
+		<div class="center-triangle"><div class="triangle"></div></div>
+		<h2>Próximos eventos</h2>
+	</div>
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="..." alt="First slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="..." alt="Second slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="..." alt="Third slide">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-icon"><</span>
+			<!--
+			<span class="carousel-control-prev-icon carousel-control-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+			-->
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+</section>
+<!--
 <h1 style="background-color: red">Modificando</h1>
 <section class="events">
-		<div class="component title">
-			<h2>Próximos eventos</h2>
-		</div>
-		<div class="elements">
-			<?php include("components/event.php");?>
-			<?php include("components/event.php");?>
-			<?php include("components/event.php");?>
-		</div>
-	</section>
+	<div class="component title">
+		<h2>Próximos eventos</h2>
+	</div>
+	<div class="elements">
+		<?php //include("components/event.php");?>
+		<?php //include("components/event.php");?>
+		<?php //include("components/event.php");?>
+	</div>
+</section>
+-->
